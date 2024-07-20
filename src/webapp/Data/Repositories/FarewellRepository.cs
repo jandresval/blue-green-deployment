7c6942481bbacefd5c91428c189f7063948fc573
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2024-07-20 10:05:49 -0400

    Implement endpoint to fetch the first record from Farewells table

---
+++ src/webapp/Data/Repositories/FarewellRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace webapp.Data.Repositories;
+
+public class FarewellRepository
+(
+    DatabaseContext databaseContext
+)
+{
+    private MySqlConnection Connection { get; } = databaseContext.Connection;
+
+    private string _farewell = string.Empty;
+
+    private const string GetFirstFarewellRecord = "SELECT farewell FROM farewells LIMIT 1;";
+
+    public string GetFarewell()
+    {
+        if (_farewell != string.Empty) return _farewell;
+
+        using var cmd = new MySqlCommand(GetFirstFarewellRecord, Connection);
+
+        using var reader = cmd.ExecuteReader();
+
+        reader.Read();
+
+        _farewell = reader.GetString(0);
+
+        return _farewell;
+    }
+}
